fc93c8cd871b4dfbb604d62e9b4630b6c35cef20
Gabriel Gagno <gjpgagno@gmail.com>
2024-11-03 22:26:35 +0800

    Added Laravel Breeze Vue with authentication

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    //darkMode: 'class', // dark mode on
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
+
     theme: {
         extend: {
             fontFamily: {
@@ -16,5 +18,6 @@ export default {
             },
         },
     },
-    plugins: [],
+
+    plugins: [forms],
 };
